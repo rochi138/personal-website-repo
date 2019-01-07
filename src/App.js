4cc82d0ce5e9c85dbb653b78d9c6fbdc0ea1df25
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-06 21:56:55 -0500

    Added Peekapak project page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from './pages/HomePage';
 import HireMePwease from './pages/HireMePwease';
 import Website from './pages/Website';
 import FowlPlay from './pages/FowlPlay';
+import Peekapak from './pages/Peekapak';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path='/' component={ Home } />
+        <Route path='/Peekapak' component={ Peekapak } />
         <Route path='/HireMePwease' component={ HireMePwease } />
         <Route path='/Website' component={ Website } />
         <Route path='/FowlPlay' component={ FowlPlay } />
+        <Route path='/Peekapak' component={ Peekapak } />
       </div>
     );
   }
