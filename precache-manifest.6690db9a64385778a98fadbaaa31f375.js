bff714126a3b5afe04738d2fe8d233aa6f477173
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-09 02:37:22 -0500

    Updates

--- precache-manifest.6690db9a64385778a98fadbaaa31f375.js
+++ precache-manifest.6690db9a64385778a98fadbaaa31f375.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "6a6d10910774eb14b9fa75cf8037c3c8",
-    "url": "/personal-website-repo/static/media/PublisherOverview.6a6d1091.jpg"
+    "revision": "adb5c45a1c802d6ec0486ea3900e4bc0",
+    "url": "/personal-website-repo/static/media/PublisherSubunit.adb5c45a.jpg"
   },
   {
-    "revision": "72fdfeb428618cb3df39",
-    "url": "/personal-website-repo/static/css/main.c93f2ed6.chunk.css"
+    "revision": "15238d85d0d5baec1e81",
+    "url": "/personal-website-repo/static/css/main.bc71a8f6.chunk.css"
   },
   {
     "revision": "78f4b1743be5809de11a3514742842d0",
     "url": "/personal-website-repo/static/media/screenshot-website-json.78f4b174.jpg"
   },
   {
-    "revision": "2cde1ac4d0df4d7b7cf6",
-    "url": "/personal-website-repo/static/js/1.2cde1ac4.chunk.js"
+    "revision": "57e9ac8eb73a8d977ce4",
+    "url": "/personal-website-repo/static/js/1.57e9ac8e.chunk.js"
   },
   {
     "revision": "831399e47b98b43e8ac4",
@@ -24,40 +24,44 @@ self.__precacheManifest = [
     "url": "/personal-website-repo/static/media/fowlPlayBanner.53e40081.jpg"
   },
   {
-    "revision": "863da0ee77c95c38d5627146cf75a5af",
-    "url": "/personal-website-repo/static/media/robynInterests.863da0ee.jpg"
+    "revision": "3e07913462eaa627cee8bb9370209bba",
+    "url": "/personal-website-repo/static/media/robynHeader.3e079134.jpg"
   },
   {
     "revision": "fe31a14070c301ba656ebf709029e191",
     "url": "/personal-website-repo/static/media/robynContact.fe31a140.jpg"
   },
   {
-    "revision": "3e07913462eaa627cee8bb9370209bba",
-    "url": "/personal-website-repo/static/media/robynHeader.3e079134.jpg"
+    "revision": "863da0ee77c95c38d5627146cf75a5af",
+    "url": "/personal-website-repo/static/media/robynInterests.863da0ee.jpg"
   },
   {
     "revision": "0a7cdae121c6969551189cae0d4f990f",
     "url": "/personal-website-repo/static/media/robynProfile.0a7cdae1.JPG"
   },
   {
-    "revision": "1c96ddcd7e5c806ef98ddc5dfa1fd41f",
-    "url": "/personal-website-repo/static/media/NewLessons.1c96ddcd.jpg"
-  },
-  {
     "revision": "b3f65e601921322196342ac862b825a0",
     "url": "/personal-website-repo/static/media/PublisherFull.b3f65e60.jpg"
   },
   {
-    "revision": "72fdfeb428618cb3df39",
-    "url": "/personal-website-repo/static/js/main.72fdfeb4.chunk.js"
+    "revision": "6a6d10910774eb14b9fa75cf8037c3c8",
+    "url": "/personal-website-repo/static/media/PublisherOverview.6a6d1091.jpg"
   },
   {
     "revision": "b882d1a7a9ff24236b087b0246a335bf",
     "url": "/personal-website-repo/static/media/PublisherStandards.b882d1a7.jpg"
   },
   {
-    "revision": "adb5c45a1c802d6ec0486ea3900e4bc0",
-    "url": "/personal-website-repo/static/media/PublisherSubunit.adb5c45a.jpg"
+    "revision": "15238d85d0d5baec1e81",
+    "url": "/personal-website-repo/static/js/main.15238d85.chunk.js"
+  },
+  {
+    "revision": "bb211f66f4b6f2ba12086b51c93f75ef",
+    "url": "/personal-website-repo/static/media/games-d20.bb211f66.jpg"
+  },
+  {
+    "revision": "97644cc0792c4520ac22230d662f1e92",
+    "url": "/personal-website-repo/static/media/golden-kamuy-scarf.97644cc0.jpg"
   },
   {
     "revision": "e61b9980e4aab4d33927f48144c120aa",
@@ -70,6 +74,10 @@ self.__precacheManifest = [
   {
     "revision": "66cb157267432917b9c0c5032419ebbf",
     "url": "/personal-website-repo/static/media/placeholder.66cb1572.jpg"
+  },
+  {
+    "revision": "600bc6cf91d7050cd70ea5f0c8ad2433",
+    "url": "/personal-website-repo/static/media/react-js.600bc6cf.jpg"
   },
   {
     "revision": "df31d96766a047e3291d8c24a4d72394",
@@ -88,11 +96,11 @@ self.__precacheManifest = [
     "url": "/personal-website-repo/static/media/screenshot-website-js.017ce07f.jpg"
   },
   {
-    "revision": "2cde1ac4d0df4d7b7cf6",
-    "url": "/personal-website-repo/static/css/1.713e3577.chunk.css"
+    "revision": "57e9ac8eb73a8d977ce4",
+    "url": "/personal-website-repo/static/css/1.2f23eae0.chunk.css"
   },
   {
-    "revision": "1008b2ec35122f45ea3badc0f00b8dc2",
+    "revision": "2917de123ed1dd5ad2aab02f0611d075",
     "url": "/personal-website-repo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website-repo/precache-manifest.aeb50e88520842b69f7bd85ff3a73e91.js"
+  "/personal-website-repo/precache-manifest.6690db9a64385778a98fadbaaa31f375.js"
 );
 
 workbox.clientsClaim();
