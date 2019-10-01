79d45f5b54541df37a2abcb953707e181d12fafc
rochi138 <robyn.ching138@gmail.com>
2019-10-01 00:55:19 -0400

    Fixed project header carry theme bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -74,13 +74,13 @@ export default class Header extends Component{
               <div style={{display: "flex", flexDirection: "row"}}>
                 <div className={ styles.barItem } onClick={ () => this.handleOnClick( 'home' ) }><i class="fa fa-home" /> HOME</div>
                 <div style={{flex: 1}} />
-                <a href={ first ? "" : "https://rochi138.github.io/personal-website-repo#page" + this.state.projectList[ this.state.i - 1 ].projectName } style={ first ? { visibility: "hidden"} : {color: "inherit"}}>
+                <div onClick={ () => this.props.history.push( first ? "/" : ("#page" + this.state.projectList[ this.state.i - 1 ].projectName ) ) } style={ first ? { visibility: "hidden"} : {color: "inherit"}}>
                   <div className={ styles.barItem }><i class="fa fa-chevron-left" /></div>
-                </a>
+                </div>
                 <div className={ styles.barItem } onClick={ () => this.setState(prevState => ({projectOpen: !prevState.projectOpen }))}><i class="fa fa-bars"/> PROJECT LIST</div>
-                <a href={ last ? "" : "https://rochi138.github.io/personal-website-repo#page" + this.state.projectList[ this.state.i + 1 ].projectName } style={ last ? { visibility: "hidden"} : {color: "inherit"}}>
+                <div onClick={ () => this.props.history.push( last ? "/" : ("#page" + this.state.projectList[ this.state.i + 1 ].projectName ) ) } style={ last ? { visibility: "hidden"} : {color: "inherit"}}>
                   <div className={ styles.barItem }><i class="fa fa-chevron-right" /></div>
-                </a>
+                </div>
                 <div style={{flex: 1}} />
                 { this.props.state.theme === "default"
                   ? <div className={ styles.barItem } onClick={ () => this.props.setState({ theme: "dark" })}><i class="fa fa-moon" /> DARK</div>
@@ -126,13 +126,13 @@ export default class Header extends Component{
               <div className={ styles.bar }>
                 <div className={ styles.barItem } onClick={ () => this.handleOnClick( 'home' ) }><i class="fa fa-home" /></div>
                 <div style={{flex: 1}} />
-                  <a href={ first ? "" : "https://rochi138.github.io/personal-website-repo#page" + this.state.projectList[ this.state.i - 1 ].projectName } style={ first ? { visibility: "hidden"} : {color: "inherit"}}>
-                    <div className={ styles.barItem }><i class="fa fa-chevron-left" /></div>
-                  </a>
+                <div onClick={ () => this.props.history.push( first ? "/" : ("#page" + this.state.projectList[ this.state.i - 1 ].projectName ) ) } style={ first ? { visibility: "hidden"} : {color: "inherit"}}>
+                  <div className={ styles.barItem }><i class="fa fa-chevron-left" /></div>
+                </div>
                 <div className={ styles.barItem } onClick={ () => this.setState(prevState => ({projectOpen: !prevState.projectOpen }))}><i class="fa fa-bars"/> PROJECT LIST</div>
-                <a href={ last ? "" : "https://rochi138.github.io/personal-website-repo#page" + this.state.projectList[ this.state.i + 1 ].projectName } style={ last ? { visibility: "hidden"} : {color: "inherit"}}>
+                <div onClick={ () => this.props.history.push( last ? "/" : ("#page" + this.state.projectList[ this.state.i + 1 ].projectName ) ) } style={ last ? { visibility: "hidden"} : {color: "inherit"}}>
                   <div className={ styles.barItem }><i class="fa fa-chevron-right" /></div>
-                </a>
+                </div>
                 <div style={{flex: 1}} />
                 { this.props.state.theme === "default"
                   ? <div className={ styles.barItem } onClick={ () => this.props.setState({ theme: "dark" })}><i class="fa fa-moon" /> </div>
